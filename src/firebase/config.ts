3f30bce7a723086e7e82b8559bac8354652e55dc
Inamar Junior <inamarjunior2@gmail.com>
2025-10-12 19:09:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7859573140-d9ca5",
+  "appId": "1:254161355825:web:1888e6ad7b4dc006cf4110",
+  "apiKey": "AIzaSyBq4a__Jyz-Md9YPW-PlddaNsYEGbOnyt8",
+  "authDomain": "studio-7859573140-d9ca5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "254161355825"
+};
